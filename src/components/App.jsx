4b72ec40d1b7faaf98dd60423ae991798dc7f877
ALLA PONOMARENKO <154559605+allaponomarenko@users.noreply.github.com>
2026-01-14 20:49:05 +0100

    6

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,22 @@
 import Product from './Product';
+import { Alert } from "./Alert";
 
 export default function App() {
   return (
     <div>
+      <Alert variant="info">
+        Would you like to browse our recommended products?
+      </Alert>
+      <Alert variant="error">
+        There was an error during your last transaction
+      </Alert>
+      <Alert variant="success">
+        Payment received, thank you for your purchase
+      </Alert>
+      <Alert variant="warning">
+        Please update your profile contact information
+      </Alert>
+
       <h1>Best selling</h1>
 
       <Product
@@ -15,7 +29,8 @@ export default function App() {
         imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
         price={14.29}
       />
-      <Product />
     </div>
   );
 }
+
+
